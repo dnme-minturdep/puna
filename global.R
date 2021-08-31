d38520a2786eea0e226f9180f732017997ce9725
Elián Soutullo/esoutullo@turismo.gob.ar <eliansoutu@gmail.com>
2021-08-31 10:55:00 -0300

    actualizo global

--- global.R
+++ global.R
@@ -1,10 +1,9 @@
 library(shiny)
 library(tidyverse)
 library(DT)
-#library(shinyWidgets)
 
 options(DT.options = list(language = list(url = '//cdn.datatables.net/plug-ins/1.10.11/i18n/Spanish.json')))
 
 serie_puna <- read_csv("/srv/DataDNMYE/puna/serie_puna.zip") %>% 
-  mutate(across(2:6, ~ case_when(is.na(.) ~ "sin dato",
+  mutate(across(2:6, ~ case_when(is.na(.) ~ "Sin dato",
                                  TRUE ~ .)))
