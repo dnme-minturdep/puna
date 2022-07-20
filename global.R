a7dfb42c9c3f26014b912ef0754147afa69ce264
eliansoutu <eliansoutul@gmail.com>
2022-07-20 12:28:45 -0300

    cambio lectura de datos a arrow

--- global.R
+++ global.R
@@ -2,12 +2,12 @@ library(shiny)
 library(tidyverse)
 library(DT)
 library(waiter)
+library(arrow)
 
 options(DT.options = list(language = list(url = '//cdn.datatables.net/plug-ins/1.10.11/i18n/Spanish.json')))
 
-serie_puna <- read_csv("/srv/DataDNMYE/puna/serie_puna.csv") %>% 
-  mutate(across(2:6, ~ case_when(is.na(.) ~ "Sin dato",
-                                 TRUE ~ .)))
+serie_puna <- read_parquet("/srv/DataDNMYE/puna/serie_puna.parquet")
+
 loading_screen <- tagList(
   h3("Cargando...", style = "color:gray;"),
   img(src = "https://tableros.yvera.tur.ar/recursos/logo_mintur_color.png", height = "200px")
